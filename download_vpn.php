d6cd57030be22c24fa3f0d825ed4ea1724085f9c
blitztide-htb <blitztide@hackthebox.com>
2023-08-14 22:28:09 +0100

    aaaa

--- download_vpn.php
+++ download_vpn.php
@@ -8,9 +8,8 @@ if(!isset($_SESSION['username'])){
   $query = sprintf("SELECT ovpn_file FROM vpn_users WHERE id='%s'",$_GET['uid']);
   $qresult = $mysqli->query($query);
   $results = mysqli_fetch_row($qresult);
-  header("Content-Type:text/plain");
+  header("Content-Type: text/plain");
   header("Content-Type: application/download");
-  header('Content-Disposition: attachment; 
-         filename="connection.ovpn"');
+  header('Content-Disposition: attachment; filename="connection.ovpn"');
   print($results[0]);
 ?>
