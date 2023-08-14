f8c03ea386418839f559802bb33617891a37baa8
blitztide-htb <blitztide@hackthebox.com>
2023-08-14 16:48:02 +0100

    aaaa

--- admin/users.php
+++ admin/users.php
@@ -35,13 +35,7 @@ if(!isset($_SESSION['username'])){
                 </div>
             </div>
         </main>
-        <footer class="footer mt-3 py-3 bg-light">
-            <div class="container">
-                <span class="text-muted">
-                    &copy; 2023 HTB
-                </span>
-            </div>
-        </footer>
+        <?php readfile("../snippets/copyright.inc");?>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </html>
