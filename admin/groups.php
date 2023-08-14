c48bb918c4c8ecc3b2b424ff6331f4f328c29d6a
blitztide-htb <blitztide@hackthebox.com>
2023-08-14 21:27:46 +0100

    aaaa

--- admin/groups.php
+++ admin/groups.php
@@ -9,6 +9,6 @@ $response['title'] = 'Groups · ADMIN';
 $response['header'] = 'ADMIN Interface';
 $response['user'] = $_SESSION['username'];
 $response['headers'] = array('#','name');
-$response['rows'] = array(array('1','2'));
+$response['rows'] = array(array('1','2'),array('3','4'));
 include '../templates/admin-table.php';
 ?>
