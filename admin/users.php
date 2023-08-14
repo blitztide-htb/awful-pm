a1178a2ec59280c49e0a446e9966d88c4e859907
blitztide-htb <blitztide@hackthebox.com>
2023-08-14 21:30:43 +0100

    aaaa

--- admin/users.php
+++ admin/users.php
@@ -5,37 +5,10 @@ if(!isset($_SESSION['username'])){
 	header('Location: /login.php');
 	exit();
 }
+$response['title'] = 'Users · ADMIN';
+$response['header'] = 'ADMIN Interface';
+$response['user'] = $_SESSION['username'];
+$response['headers'] = array('#','name');
+$response['rows'] = array(array('1','2'),array('3','4'));
+include '../templates/admin-table.php';
 ?>
-<!DOCTYPE html>
-<html class="h-100">
-    <head>
-        <?php readfile("../snippets/header.inc");?>
-        <title> Users · ADMIN</title>
-    </head>
-    <body class="d-flex flex-column h-100">
-        <?php readfile("../snippets/admin-nav.inc");?>
-        <main class="flex-fill w-75 mx-auto">
-            <div class="container mt-5">
-                <div class="card p-5">
-                    <h1> Users</h1>
-                    <div class="table-responsive">
-                        <table class="table table-sm">
-                            <tr>
-                                <th>#</th>
-                                <th>Username</th>
-                                <th>Password</th>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>admin</td>
-                                <td>password</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <?php readfile("../snippets/copyright.inc");?>
-    </body>
-    <?php readfile("../snippets/footer.inc");?>
-</html>
--- admin/vpns.php
+++ admin/vpns.php
@@ -5,37 +5,10 @@ if(!isset($_SESSION['username'])){
 	header('Location: /login.php');
 	exit();
 }
+$response['title'] = 'VPNs · ADMIN';
+$response['header'] = 'ADMIN Interface';
+$response['user'] = $_SESSION['username'];
+$response['headers'] = array('#','name');
+$response['rows'] = array(array('1','2'),array('3','4'));
+include '../templates/admin-table.php';
 ?>
-<!DOCTYPE html>
-<html class="h-100">
-    <head>
-        <?php readfile("../snippets/header.inc");?>
-        <title> VPNS · ADMIN</title>
-    </head>
-    <body class="d-flex flex-column h-100">
-        <?php readfile("../snippets/admin-nav.inc");?>
-        <main class="flex-fill w-75 mx-auto">
-            <div class="container mt-5">
-                <div class="card p-5">
-                    <h1> VPNs</h1>
-                    <div class="table-responsive">
-                        <table class="table table-sm">
-                            <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th></th>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>Corporate</td>
-                                <td></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </main>
-        <?php readfile("../snippets/copyright.inc");?>
-    </body>
-    <?php readfile("../snippets/footer.inc");?>
-</html>
