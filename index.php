c4ba35f3fb207ed7a9e5b819ab27a124dba47c67
blitztide-htb <blitztide@hackthebox.com>
2023-08-15 12:12:40 +0100

    aaaa

--- index.php
+++ index.php
@@ -73,8 +73,10 @@ include './templates/header.php';
             <h1> Connections</h1>
             Use these OVPN files to connect to all the systems where you are authorized.
             <ul class="list-group mt-2">
-                <li class="list-group-item d-flex justify-content-between align-items-center">Corporate VPN <a href="./download_vpn.php?uid=<?php print($_SESSION['uid']);?>" class="btn btn-success mr-0"><i class="bi bi-download"></i>
+                <?php foreach($vpns as $vpn): ?>
+                <li class="list-group-item d-flex justify-content-between align-items-center"><?=$vpn->name?><a href="./download_vpn.php?id=<?=$vpn->id?>" class="btn btn-success mr-0"><i class="bi bi-download"></i>
                 </a></li>
+                <?php endforeach;?>
             </ul>
         </div>
     </div>
