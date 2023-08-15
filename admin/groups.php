cabd10e6f34a2870b368e6bc4ec800322bafd9d2
blitztide-htb <blitztide@hackthebox.com>
2023-08-15 11:36:16 +0100

    aaaa

--- admin/groups.php
+++ admin/groups.php
@@ -6,11 +6,25 @@ if(!isset($_SESSION['username'])){
 	header('Location: /login.php');
 	exit();
 }
+
+// Get all groups
+$query = sprintf("SELECT * FROM groups");
+$result = $mysqli->query($query);
+if(!$result) {
+    die(mysqli_error($mysqli));
+}
+
+$response['headers'] = array();
+$fields = mysqli_fetch_fields($result);
+foreach($fields as $field) {
+    $response['headers'][] = $field->name;
+}
+
+$response['rows'] = mysqli_fetch_all($result, MYSQLI_NUM);
+
 $response['title'] = 'Groups · ADMIN';
 $response['header'] = 'ADMIN Interface';
 $response['user'] = $_SESSION['username'];
 $response['table-title'] = 'Groups';
-$response['headers'] = array('#','name');
-$response['rows'] = array(array('1','2'),array('3','4'));
 include '../templates/admin-table.php';
 ?>
