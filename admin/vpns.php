3eef505d841bc560eda5c45e7a736994b3a5d51b
blitztide-htb <blitztide@hackthebox.com>
2023-08-15 11:37:01 +0100

    aaaa

--- admin/vpns.php
+++ admin/vpns.php
@@ -6,6 +6,22 @@ if(!isset($_SESSION['username'])){
 	header('Location: /login.php');
 	exit();
 }
+
+// Get all VPNs
+$query = sprintf("SELECT * FROM VPNs");
+$result = $mysqli->query($query);
+if(!$result) {
+    die(mysqli_error($mysqli));
+}
+
+$response['headers'] = array();
+$fields = mysqli_fetch_fields($result);
+foreach($fields as $field) {
+    $response['headers'][] = $field->name;
+}
+
+$response['rows'] = mysqli_fetch_all($result, MYSQLI_NUM);
+
 $response['title'] = 'VPNs · ADMIN';
 $response['header'] = 'ADMIN Interface';
 $response['user'] = $_SESSION['username'];
