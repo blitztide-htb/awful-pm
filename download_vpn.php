faebd3712842cd2c7151832a18df8ae720bb7bd3
David Elliott <blitztide@hackthebox.com>
2023-08-22 10:33:32 +0100

    Added hint for id parameter

--- download_vpn.php
+++ download_vpn.php
@@ -5,14 +5,19 @@ if(!isset($_SESSION['username'])){
 	header('Location: /login.php');
 	exit();
 }
-  $query = sprintf("SELECT config FROM vpns WHERE id='%s'",$_GET['id']);
-  $qresult = $mysqli->query($query);
-  $results = mysqli_fetch_row($qresult);
-  if(!$results[0]){
-    die("NO VPN FILE FOUND");
-  }
-  header("Content-Type: text/plain");
-  header("Content-Type: application/download");
-  header('Content-Disposition: attachment; filename="connection.ovpn"');
-  print($results[0]);
+
+if(!isset($_GET['id']))
+{
+    die('Please provide id parameter');
+}
+$query = sprintf("SELECT config FROM vpns WHERE id='%s'",$_GET['id']);
+$qresult = $mysqli->query($query);
+$results = mysqli_fetch_row($qresult);
+if(!$results[0]){
+  die("NO VPN FILE FOUND");
+}
+header("Content-Type: text/plain");
+header("Content-Type: application/download");
+header('Content-Disposition: attachment; filename="connection.ovpn"');
+print($results[0]);
 ?>
